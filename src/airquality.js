ec4eb609341928fdc5febf27ef2cdd2c9c293996
Francisco Barron <franz.barron@outlook.com>
2020-08-24 18:48:12 -0500

    First commit

---
+++ src/airquality.js
@@ -0,0 +1,85 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function getAirQuality(latitude, longitude) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const airQualityData = yield fetch(`${baseURL}/air-quality/${latitude},${longitude}`)
+            .then((data) => data.json())
+            .catch((e) => console.error(`An error ocurred: ${e.message}`));
+        const stationCoords = airQualityData.data.city.geo;
+        if (calculateDistance(latitude, longitude, stationCoords[0], stationCoords[1]) <= 10)
+            showAirQuality(airQualityData);
+        else
+            invalidStation();
+    });
+}
+function showAirQuality(data) {
+    showQualityLevel(data.data.aqi);
+    showQualityDescription(data.data.aqi);
+}
+function showQualityLevel(index) {
+    const qualityLevelElt = document.querySelector('#quality-level');
+    if (index <= 50) {
+        qualityLevelElt.textContent = 'Good';
+        qualityLevelElt.classList.value = 'aq-0';
+    }
+    if (index > 50 && index <= 100) {
+        qualityLevelElt.textContent = 'Moderate';
+        qualityLevelElt.classList.value = 'aq-1';
+    }
+    if (index > 100 && index <= 150) {
+        qualityLevelElt.textContent = 'Unhealthy for sensitive groups';
+        qualityLevelElt.classList.value = 'aq-2';
+    }
+    if (index > 150 && index <= 200) {
+        qualityLevelElt.textContent = 'Unhealthy';
+        qualityLevelElt.classList.value = 'aq-3';
+    }
+    if (index > 200 && index <= 300) {
+        qualityLevelElt.textContent = 'Very unhealthy';
+        qualityLevelElt.classList.value = 'aq-4';
+    }
+    if (index > 300) {
+        qualityLevelElt.textContent = 'Hazardous';
+        qualityLevelElt.classList.value = 'aq-5';
+    }
+}
+function showQualityDescription(index) {
+    const qualityDescriptionElt = document.querySelector('#quality-description');
+    if (index <= 50) {
+        qualityDescriptionElt.textContent =
+            'Air quality is considered satisfactory, and air pollution poses little or no risk';
+    }
+    if (index > 50 && index <= 100) {
+        qualityDescriptionElt.textContent =
+            'Air quality is acceptable; however, there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.';
+    }
+    if (index > 100 && index <= 150) {
+        qualityDescriptionElt.textContent =
+            'Members of sensitive groups may experience health effects. The general public is not likely to be affected.';
+    }
+    if (index > 150 && index <= 200) {
+        qualityDescriptionElt.textContent =
+            'Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects';
+    }
+    if (index > 200 && index <= 300) {
+        qualityDescriptionElt.textContent =
+            'Health warnings of emergency conditions. The entire population is more likely to be affected.';
+    }
+    if (index > 300) {
+        qualityDescriptionElt.textContent =
+            'Health alert. Everyone may experience more serious health effects';
+    }
+}
+function invalidStation() {
+    const qualityLevelElt = document.querySelector('#quality-level');
+    qualityLevelElt.classList.value = '';
+    qualityLevelElt.textContent = 'N/A';
+    document.querySelector('#quality-description').textContent = `It seems like there's no information for this location's air quality`;
+}
---
+++ src/main.js
@@ -0,0 +1,97 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+let iconsList;
+let units = 'metric';
+const baseURL = 'https://weather-air-quality-app.herokuapp.com/';
+fetch(baseURL).then(() => {
+    console.log('Server awake');
+});
+(() => __awaiter(this, void 0, void 0, function* () {
+    iconsList = yield fetch('./src/icons.json').then((data) => data.json());
+}))();
+function gotPosition(position) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const { latitude, longitude } = position.coords;
+        const locationData = yield fetch(`${baseURL}/geocode/${latitude},${longitude}`)
+            .then((data) => data.json())
+            .catch((err) => {
+            console.error(`An error ocurred: ${err}`);
+            if (err.message === 'Failed to fetch')
+                showError(`There was a problem connecting to the web.`);
+            return;
+        });
+        const parsedLocation = parseLocation(locationData.results[0].components);
+        showLocation(parsedLocation);
+        document.querySelector('#location-data').classList.remove('hidden');
+        document.querySelector('#error').classList.add('hidden');
+        getWeatherData(latitude, longitude);
+        getAirQuality(latitude, longitude);
+    });
+}
+function parseLocation(data) {
+    var _a, _b;
+    const thirdLevel = ((_a = data.city) !== null && _a !== void 0 ? _a : data.county);
+    const secondLevel = ((_b = data.state) !== null && _b !== void 0 ? _b : '');
+    const firstLevel = data.country;
+    return { firstLevel, secondLevel, thirdLevel };
+}
+function showError(error) {
+    document.querySelector('#location-data').classList.add('hidden');
+    document.querySelector('#error').classList.remove('hidden');
+    document.querySelector('#error-message').textContent = error;
+    document.querySelector('#loading').classList.add('hidden');
+}
+function getLocationCoords(locationName) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const locationData = yield fetch(`${baseURL}/geocode/${locationName}`)
+            .then((data) => data.json())
+            .catch((err) => {
+            console.error(`An error ocurred: ${err}`);
+            if (err.message === 'Failed to fetch')
+                showError(`There was a problem connecting to the web.`);
+            return;
+        });
+        if (locationData.results.length === 0) {
+            console.error(`An error ocurred: No data found for ${locationName}`);
+            showError(`Sorry! We couldn't find a place called ${locationName}.`);
+            return;
+        }
+        const { lat, lng } = locationData.results[0].geometry;
+        const parsedLocation = parseLocation(locationData.results[0].components);
+        showLocation(parsedLocation);
+        document.querySelector('#location-data').classList.remove('hidden');
+        document.querySelector('#error').classList.add('hidden');
+        getWeatherData(lat, lng);
+        getAirQuality(lat, lng);
+    });
+}
+function showLocation(data) {
+    let locationName = '';
+    if (data.thirdLevel)
+        locationName += `${data.thirdLevel}, `;
+    if (data.secondLevel)
+        locationName += `${data.secondLevel}, `;
+    locationName += `${data.firstLevel}`;
+    document.querySelector('#location').textContent = locationName;
+    document.querySelector('#loading').classList.add('hidden');
+}
+function calculateDistance(lat1, lon1, lat2, lon2) {
+    const R = 6371;
+    const dLat = ((lat2 - lat1) * Math.PI) / 180;
+    const dLon = ((lon2 - lon1) * Math.PI) / 180;
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos((lat1 * Math.PI) / 180) *
+            Math.cos((lat2 * Math.PI) / 180) *
+            Math.sin(dLon / 2) *
+            Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c;
+    return d;
+}
